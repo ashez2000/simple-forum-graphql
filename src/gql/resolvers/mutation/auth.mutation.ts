0ef2b49b7e4c48a8659682030ab780be692d9542
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2023-02-04 04:29:21 +0530

    feat: Sending JWT token on auth

--- src/gql/resolvers/mutation/auth.mutation.ts
+++ src/gql/resolvers/mutation/auth.mutation.ts
@@ -1,3 +1,4 @@
+import jwt from 'jsonwebtoken';
 import db from '../../../db/prisma';
 
 type UserCredential = {
@@ -12,19 +13,20 @@ type UserCreateData = {
 // TODO: Input validation
 // TODO: Password hashing
 // TODO: Proper response and error handling
+// TODO: JWT func
 export const AuthMutation = {
   signup: async (parent: any, args: UserCreateData, ctx: any, info: any) => {
-    await db.user.create({ data: args });
-    return 'Signup Successful';
+    const user = await db.user.create({ data: args });
+    const token = jwt.sign({ id: user.id }, 'asdfjkl;');
+    return token;
   },
 
   signin: async (parent: any, args: UserCredential, ctx: any, info: any) => {
     const { email } = args;
-
     const user = await db.user.findUnique({ where: { email } });
     if (!user) return 'Invalid Credentials';
     if (user.password !== args.password) return 'Invalid Credentials';
-
-    return 'Signin Successful';
+    const token = jwt.sign({ id: user.id }, 'asdfjkl;');
+    return token;
   },
 };
